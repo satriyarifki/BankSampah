49d593aed3f448ce52f5f0223f430b2f7dbec92f
satriyarp <rifkisatriya@gmail.com>
2022-06-29 09:40:20 +0700

    go

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,40 @@
 @extends('layouts.admin')
 @section('content')
-<div class="content">
-    <div class="row">
-        <div class="col-lg-12">
-            <div class="card">
-                <div class="card-header">
-                    Dashboard
-                </div>
-
-                <div class="card-body">
-                    @if(session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+<header class="masthead" style="background-image: url('https://wallpaperaccess.com/plastic-pollution')">
+    <div class="container position-relative px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <div class="page-heading">
+                    <h1>Bank Sampah</h1>
+                    <span class="subheading">This is what I do.</span>
                 </div>
             </div>
         </div>
     </div>
-</div>
+</header>
+<!-- Main Content-->
+<main class="mb-4">
+    <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <p>Plastik yang bisa didaur ulang dapat dikenali dari warna. Warna plastik yang bening, dan tembus pandang lebih mudah untuk di daur ulang. Sementara plastik yang memiliki lapisan lain, seperti plastik kemasan lebih sulit di daur ulang.</p>
+                <p>Plastik yang berwarna tadai dapat dimanfaatkan untuk kepentingan lain seperti dibuat bahan kerajinan, atau hal lain. Cara membuat kerajinan ini sebenarnya cukup mudah, dan bisa didapatkan tutorialnya melalui YouTube.</p>
+                <p>Sementara untuk menghemat ruang dalam penggunaan sampah plastik bisa dengan cara melipat bungkus kemasan. Melipat ini bisa menghemat banyak ruang untuk tempat sampah.</p>
+                <p>Sampah plastik penting dipisahkan karena tidak dapat didaur ulang selama ratusan, bahkan ribuan tahun. Sampah plastik sebaiknya tidak dibakar karena dapat menghasilkan gas emisi CO2 sehingga udara sekitar menjadi tidak sehat. Cara yang paling efektif memilah sampah plastik, memang dengan mengkategorisasi sesuai kebutuhan dan jenisnya</p>
+            </div>
+        </div>
+    </div>
+</main>
+<!-- Footer-->
+<footer class="border-top">
+    <div class="container px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                
+            </div>
+        </div>
+    </div>
+</footer>
 @endsection
 @section('scripts')
 @parent
